835ac06746ba481a1e940974768ec83db03dcd1f
samys003 <samara_rolim010@hotmail.com>
2025-11-03 01:43:35 -0300

    finalizando a criação das funções do wpp para serem criadas as rotas

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -12,31 +12,115 @@
 const dados = require('./contatos.js')
 
 
-const getAllUsuarios =  function(){
-    let message = { status: true, status_code: 200, development: 'Samara Santos' , dados: dados.contatos['whats-users'] }
+const getAllUsuarios = function () {
+  let message = { status: true, status_code: 200, development: 'Samara Santos', dados: dados.contatos['whats-users'] }
 
-    return message
-    
+  return message
+
 }
 
-const getProfileUser = function(id){
+const getProfileUser = function (id) {
 
-     let message = { status: true, status_code: 200, development: 'Samara Santos' , profile: [] }
+  let message = { status: true, status_code: 200, development: 'Samara Santos', profile: [] }
 
-     dados.contatos['whats-users'].forEach(item => {
-        if(item === id)
+  dados.contatos['whats-users'].forEach(item => {
+    if (item.id === id) {
 
-          message.profile.push({
-            nome: item.nickname,
-            numero: item.number,
-            foto: item['profile-image'],
-            background: item.background,
-            criado_em: item['created-since']
+      message.profile.push({
+        nome: item.nickname,
+        numero: item.number,
+        foto: item['profile-image'],
+        background: item.background,
+        criado_em: item['created-since']
+      })
+
+    }
+  })
+  return message
+}
+
+const getContactUser = function (number) {
+
+  let message = { status: true, status_code: 200, development: 'Samara Santos' }
+
+  dados.contatos['whats-users'].forEach(item => {
+    if (item.number === number) {
+
+      message.number = item.number
+      message.contatos = item.contacts.map(user => {
+        return {
+          nome: user.name,
+          numero: user.number,
+          imagem: user.image,
+          descricao: user.description
+
+        }
+
+      })
+
+    }
+
+  })
+  return message
+}
+
+const getAllMessages = function (number) {
+
+  let message = { status: true, status_code: 200, development: 'Samara Santos' }
+
+  dados.contatos['whats-users'].forEach(item => {
+    if (item.number === number) {
+
+
+
+      message.mensagens = item.contacts.map(user => {
+        return user.messages
+
+
+      })
+
+    }
+  })
+  return message
+}
+
+const getUserMessage = function (number) {
+
+  let message = { status: true, status_code: 200, development: 'Samara Santos' }
+
+  dados.contatos['whats-users'].forEach(item => {
+    if (item.number === number) {
+
+
+      message.contato = item.contacts.map(user => {
+        return {
+          nome: user.name,
+          numero: user.number,
+          content: user.messages.map(contact => {
+            return {
+              nome: contact.sender,
+              mensagem: contact.content,
+              hora: contact.time
+            }
+
           })
-        
-          return message
-    })
-    
+        }
+      })
+
+    }
+
+  })
+
+  return message
 }
 
-console.log(getProfileUser(1))
+
+
+module.exports = {
+  getAllUsuarios,
+  getProfileUser,
+  getContactUser,
+  getAllMessages,
+  getUserMessage
+
+}
